64fb509b7e29c85e1b3c07caf29ddcc4a1cbf2d6
abishek-sundar <abisheksundar2@gmail.com>
2020-10-30 15:59:46 +0400

    Moved some stuff around

--- node-backend/scripts/runNodeMongo.sh
+++ node-backend/scripts/runNodeMongo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1
+cd ..
 mongo_count=$(ps -ax | grep mongo -c)
 if [ $mongo_count == 1 ]
 then
--- node-backend/scripts/stopMongo.sh
+++ node-backend/scripts/stopMongo.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 pid=$(ps -ax | grep mongo | awk '{print $1}'| head -n 1)
 kill $pid
+exit 0
